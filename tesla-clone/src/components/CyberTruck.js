9391ad48b6b3ec3f715521be3252334d499cfc03
Song Jaewoo <xoals19958@gmail.com>
2021-07-21 16:18:35 +0900

    0721
    Working (CyberTruck)

--- tesla-clone/src/components/CyberTruck.js
+++ tesla-clone/src/components/CyberTruck.js
@@ -7,7 +7,16 @@ function CyberTruck() {
             <CyberTruck_CT>
                 <Main>
                     <Title src="/images/cybertruck_logo.png" />
+                    <TitleBottom>
+                        <p>Better Utility than a truck with More Performance than a sports car</p>
+                        <TitleBottomBtn>ORDER NOW</TitleBottomBtn>
+                    </TitleBottom>
                 </Main>
+                <Description>
+                    <img src="https://www.tesla.com/xNVh4yUEc3B9/02_Desktop.jpg" alt="" />
+                    <h1>EXOSKELETON</h1>
+                </Description>
+
             </CyberTruck_CT>
         </div>
     )
@@ -37,6 +46,39 @@ const Title = styled.img `
     margin-top: 2rem;
     width: 50vh;
     height: 35vh;
+`
+
+const TitleBottom = styled.div `
+padding-bottom: 10vh;
+letter-spacing: 2px;
+display: flex;
+flex-direction: column;
+align-items: center;
+p{
+    font-size: 0.9rem;
+    color: white;
+    font-weight: 600;
+    text-shadow: 1px 1px 10px ;
+    text-transform: uppercase;
+}
+
+`
+
+const TitleBottomBtn = styled.p `
+    margin-top: 5vh;
+    border: 4px solid white;
+    padding: 5px;
+    width: 150px;
     
+`
+
+const Description = styled.div `
+    background-color: black;
+    
+    img {
+        display: block;
+        padding-top: 3rem;
+        margin: 0px auto;
+    }
     
 `
